a4eaf8f11f50ccc60d5cf48da3302f63e23e7c63
Victor Renzo <v.renzo@usp.br>
2022-07-26 20:33:43 -0300

    tabuleiro finalizado

--- Modalidade Programação/Nível Júnior/2017/Fase 2/Tabuleiro/Tabuleiro.java
+++ Modalidade Programação/Nível Júnior/2017/Fase 2/Tabuleiro/Tabuleiro.java
@@ -1,4 +1,4 @@
-/* não está pronto, pretendo resolver com recursão/backtracking */
+/* Passa nos testes dados, mas estou com dificuldade em dizer se está realmente correto */
 import java.util.Scanner;
 
 public class Tabuleiro{
@@ -7,7 +7,8 @@ public class Tabuleiro{
     public static void main(String[] args){
         int N = sc.nextInt();
         int[][] tabuleiro = inicializaTabuleiro(N);
-         
+        System.out.println(preencheCelula(N-1, tabuleiro)); 
+        //imprimeTabuleiro(tabuleiro);        
     }
   
     static int[][] inicializaTabuleiro(int N){
@@ -21,15 +22,13 @@ public class Tabuleiro{
         return tabuleiro;
     }
     
-    static int preencheCelula (int N, int[] tabuleiro){
-        boolean haMaisBrancosVizinhos = temMaisBrancos();
-        if (estaVazio(N-1)) preencheCelula(N-1, tabuleiro);
-        else
-        if (haMaisBrancosVizinhos) return 1;
-        else if (tabuleiro[N-1][N] == 9) preencheCelula
+    static int preencheCelula (int N, int[][] tabuleiro){
+        if (temMaisBrancos(N, tabuleiro)) return 1;
+        else if (!temMaisBrancos(N, tabuleiro) && !temVazio(N, tabuleiro)) return 0;  
+        else return preencheCelula(N-1, tabuleiro);
     }
 
-    static boolean temMaisBrancos (int N, int[] tabuleiro){
+    static boolean temMaisBrancos (int N, int[][] tabuleiro){
         boolean possibilidade1 = tabuleiro[N-1][N] == 0 && tabuleiro[N][N-1] == 0;
         boolean possibilidade2 = tabuleiro[N-1][N-1] == 0 && tabuleiro[N][N-1] == 0;
         boolean possibilidade3 = tabuleiro[N-1][N-1] == 0 && tabuleiro[N-1][N] == 0;
@@ -37,7 +36,17 @@ public class Tabuleiro{
         else return false;
     }
 
-    static boolean estaVazio (int N, int[] tabuleiro){
-        if (tabuleiro[N][N])
+    static boolean temVazio (int N, int[][] tabuleiro){
+        if (tabuleiro[N-1][N] != 9 && tabuleiro[N-1][N-1] != 9 && tabuleiro[N][N-1] != 9) return false;
+        else return true;
     }
+
+    /*static void imprimeTabuleiro (int[][] tabuleiro){
+        for (int i = 0; i < tabuleiro.length; i++){
+            for (int j = 0; j < tabuleiro.length; j++){
+                System.out.print(tabuleiro[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+    }*/
 }
